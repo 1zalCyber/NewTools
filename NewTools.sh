199a0fd419240fba8b3c4b463c7f0b84fc28addf
1zalCyber <54761029+1zalCyber@users.noreply.github.com>
2019-09-08 11:46:12 +0700

    Add files via upload

---
+++ NewTools.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+clear
+figlet "1zalCyber" | lolcat
+sleep 5
+read -p  "Masukan Nickname :" nama
+echo "Hai Kak $nama,Jomblo Ya Wkwk"
+sleep 3
+clear
+figlet "wellcome" | lolcat
+sleep 1
+echo "Gunakan Dengan Bijak Dan Jangan Diperjual Belikan"
+sleep 1
+echo "      _,.-------.,_
+        ,;~'             '~;,
+      ,;                     ;,
+     ;                         ;
+    ,'                         ',
+   ,;                           ;,
+   ; ;      .           .      ; ;
+   | ;   ______       ______   ; |
+   |  '/~'     ~' . '~     '~\'  |
+   |  ~  ,-~~~^~, | ,~^~~~-,  ~  |
+    |   |        }:{        |   |
+    |   l       / | \       !   |
+    .~  (__,.--' .^. '--.,__)  ~.
+    |     ---;' / | \ ';---     |
+     \__.       \/^\/       .__/
+      V| \                 / |V
+       | |T~\___!___!___/~T| |
+       | |'IIII_I_I_I_IIII'| |
+       |  \,III I I I III,/  |
+        \   '~~~~~~~~~~'    /
+          \   .       .   /
+            \.    ^    ./
+              ^~~~^~~~^ " | lolcat
+echo "<==============================>
+Author : 1zalCyber
+Email : faishalwalker@gmail.com
+Whatsapp : 08**********
+<==============================>" | lolcat
+sleep 2
+echo "---------------------------------------------------------"
+echo "Apakah Anda Yakin Ingin Melanjutkan?(y/n)"
+read pilih
+if [ pilih = "y" ] || [ pilih = y ]
+then
+sleep 2
+clear
+sleep 2
+echo "      _,.-------.,_
+        ,;~'             '~;,
+      ,;                     ;,
+     ;                         ;
+    ,'                         ',
+   ,;                           ;,
+   ; ;      .           .      ; ;
+   | ;   ______       ______   ; |
+   |  '/~'     ~' . '~     '~\'  |
+   |  ~  ,-~~~^~, | ,~^~~~-,  ~  |
+    |   |        }:{        |   |
+    |   l       / | \       !   |
+    .~  (__,.--' .^. '--.,__)  ~.
+    |     ---;' / | \ ';---     |
+     \__.       \/^\/       .__/
+      V| \                 / |V
+       | |T~\___!___!___/~T| |
+       | |'IIII_I_I_I_IIII'| |
+       |  \,III I I I III,/  |
+        \   '~~~~~~~~~~'    /
+          \   .       .   /
+            \.    ^    ./
+              ^~~~^~~~^ " | lolcat
+echo "<==============================>
+Author : 1zalCyber
+Email : faishalwalker@gmail.com
+Whatsapp : 08**********
+<==============================>" | lolcat
+fi
+echo "Silahkan Pilih Nomor:"
+echo "[1] Spam Call"
+echo "[2] Spam Sms"
+read -p "Masukan Disini:" nomor
+if [ $nomor = 1 ] || [ $eue = 01 ]
+then
+clear
+sleep 1
+figlet "WAIT..." | lolcat
+pkg install php -y
+pkg install git
+git clone https://github.com/ClayHackerTeam/311
+cd 311
+php SpamCall.php
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+sleep 1
+figlet "WAIT..." | lolcat
+pkg install php -y
+pkg install git
+git clone https://github.com/alfianokt/caping-spam
+cd caping-spam
+php run.php
+fi
